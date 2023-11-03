7fd35fa4ac0d250525104fdf8820378298ce56a2
Rostyslav <141353047+savage0003@users.noreply.github.com>
2023-11-03 14:50:16 +0000

    added cart and login_required

--- users/models.py
+++ users/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from PIL import Image
 
 from django.contrib.auth.models import AbstractUser
 
+def crop_to_square(image):
+    min_size = min(image.width, image.height)
+    image = image.crop((0, 0, min_size, min_size))
+
+    return image
 class User(AbstractUser):
     image = models.ImageField(upload_to='users_images', null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.image:
+            img = Image.open(self.image.path)
+            img = crop_to_square(img)
+            img.save(self.image.path)
--- users/views.py
+++ users/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.contrib import auth, messages
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from users.models import User
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from products.models import Basket
 
 def login(request):
     if request.method == 'POST':
@@ -30,7 +32,7 @@ def registration(request):
         form = UserRegistrationForm()
     context = {'form': form}
     return render(request, 'users/registration.html', context)
-
+@login_required
 def profile(request):
     if request.method == 'POST':
         form = UserProfileForm(instance=request.user, data=request.POST, files=request.FILES)
@@ -42,9 +44,13 @@ def profile(request):
             print(form.errors)
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'Profile', 'form': form}
+    context = {
+        'title': 'Profile',
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user),
+    }
     return render(request, 'users/profile.html', context)
-
+@login_required
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
