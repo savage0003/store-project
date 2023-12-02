2543509750ff0d59873596fcb508633af1c07535
Rostyslav <141353047+savage0003@users.noreply.github.com>
2023-12-02 13:23:09 +0000

    added order-create.html

--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,17 @@
 from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-class OrderCreateView(CreateView):
+from common.views import TitleMixin
+from orders.forms import OrderForm
+
+
+class OrderCreateView(TitleMixin, CreateView):
     template_name = 'orders/order-create.html'
+    form_class = OrderForm
+    success_url = reverse_lazy('orders:order_create')
+    title = 'Store - Placing an order'
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return super(OrderCreateView, self).form_valid(form)
     
